d2008dbb42656ef2a93e2bfc8d47a4e75f71e685
anhongkui <anhongkui@jd.com>
2024-01-25 14:36:18 +0000

    update

--- pkg/grpc/client.go
+++ pkg/grpc/client.go
@@ -1,5 +1,15 @@
 package grpc
 
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/anhk/mtun/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
 type ClientOption struct {
 	Token      string // token to authenticate
 	ServerAddr string // the address of server
@@ -7,8 +17,24 @@ type ClientOption struct {
 }
 
 type GrpcClient struct {
+	c      grpc.ClientConnInterface
+	client proto.StreamClient
+	stream proto.Stream_PersistentStreamClient
 }
 
-func StartGrpcClient(option *ClientOption) {
+func StartGrpcClient(option *ClientOption) *GrpcClient {
+	token := &TokenAuth{}
+	token.T = time.Now().Unix()
+	token.S = token.Sign(option.Token)
 
+	c, err := grpc.Dial(fmt.Sprintf("%s:%d", option.ServerAddr, option.ServerPort),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithPerRPCCredentials(token))
+	check(err)
+
+	client := proto.NewStreamClient(c)
+	stream, err := client.PersistentStream(context.Background())
+	check(err)
+
+	return &GrpcClient{c: c, client: client, stream: stream}
 }
